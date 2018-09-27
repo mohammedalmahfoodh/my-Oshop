b0860a857b738693216ed77f48cff2122a7a4504
mohammed <mohammedalmahfoodh@yahoo.com>
2018-09-27 15:09:50 +0200

    develope auth service component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
 import * as firebase from 'firebase'
 import { concat, Observable } from "rxjs";
+import { ActivatedRoute } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   user$:Observable <firebase.User>;
-  constructor(private afAuth:AngularFireAuth) {
+  constructor(private afAuth:AngularFireAuth,private route:ActivatedRoute) {
     this.user$=afAuth.authState;
    }
    
   //### log in for admin with email and password ########
   doRegister(value){
+    let returnUrl= this.route.snapshot.queryParamMap.get('returnUrl') ||'/admin-login'
+  localStorage.setItem('returnUrl',returnUrl)
     return new Promise<any>((resolve, reject) => {
       firebase.auth().signInWithEmailAndPassword(value.email, value.password)
       .then(res => {
@@ -23,6 +26,7 @@ export class AuthService {
   
 ///##### login for user with google email #######
   login(){
+  
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())     
   }
   logout(){
