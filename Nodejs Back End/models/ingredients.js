bc490c587168abfe60e6615363f829c691f04bee
mohammed <mohammedalmahfoodh@yahoo.com>
2018-09-27 17:16:39 +0200

    create ingredient schema

--- Nodejs Back End/models/ingredients.js
+++ Nodejs Back End/models/ingredients.js
@@ -3,4 +3,4 @@ const schema=mongoose.Schema
 const ingredientSchema=new schema({
     Nummer:string,Namn:string,ViktGram:string,Huvudgrupp:string,Naringsvarden:{
         Naringsvarde:[{
-            Namn:string,Varde:number  } ]    }                         })
+            Namn:string,Varde:string,Enhet:string  } ]    }                         })
